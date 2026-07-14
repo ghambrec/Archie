e610d9cabfcf4bbcb137bd40db8a77c0584deb27
ghambrec <gabriel.hambrecht@gmail.com>
2026-07-14 17:11:56 +0200

    added swagger, reachable at :3000/docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -15,6 +16,16 @@ async function bootstrap() {
 		transform: true
 	})
   );
+
+  const config = new DocumentBuilder()
+  	.setTitle('Archie API')
+	.setDescription('API documentation for the archie project')
+	.setVersion('0.1')
+	.addBearerAuth()
+	.build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
