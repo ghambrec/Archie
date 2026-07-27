2a1280ec69b6dc7447e4a9fb56071e1f48c125df
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-07-27 19:19:04 +0200

    Add basic swagger

--- backend/nest-server/src/main.ts
+++ backend/nest-server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documents System API')
+    .setDescription('API documentation for the Documents System backend')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
