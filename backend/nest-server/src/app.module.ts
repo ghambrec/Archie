fde2f098a2e1a77c209901665ee4ab8b9d61d0ea
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-07-27 18:15:46 +0200

    Create modules foldder for nestjs in src

--- backend/nest-server/src/app.module.ts
+++ backend/nest-server/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { GroupsModule } from './groups/groups.module';
-import { PermissionsModule } from './permissions/permissions.module';
-import { UserGroupsModule } from './user-groups/user-groups.module';
-import { GroupPermissionsModule } from './group-permissions/group-permissions.module';
-import { DocumentsModule } from './documents/documents.module';
-import { DocumentGroupsModule } from './document-groups/document-groups.module';
+import { UsersModule } from './modules/users/users.module';
+import { GroupsModule } from './modules/groups/groups.module';
+import { PermissionsModule } from './modules/permissions/permissions.module';
+import { UserGroupsModule } from './modules/user-groups/user-groups.module';
+import { GroupPermissionsModule } from './modules/group-permissions/group-permissions.module';
+import { DocumentsModule } from './modules/documents/documents.module';
+import { DocumentGroupsModule } from './modules/document-groups/document-groups.module';
 
 @Module({
   imports: [UsersModule, GroupsModule, PermissionsModule, UserGroupsModule, GroupPermissionsModule, DocumentsModule, DocumentGroupsModule],
