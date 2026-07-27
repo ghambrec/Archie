6fb3f1291aed4c89585303c4f211fc6ca84ac09e
poverbec <poverbec@student.42heilbronn>
2026-07-27 16:32:16 +0200

    backend nest-server TypeOrm initalized

--- backend/nest-server/src/app.module.ts
+++ backend/nest-server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { UserGroupsModule } from './user-groups/user-groups.module';
+import { GroupPermissionsModule } from './group-permissions/group-permissions.module';
+import { DocumentsModule } from './documents/documents.module';
+import { DocumentGroupsModule } from './document-groups/document-groups.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, GroupsModule, PermissionsModule, UserGroupsModule, GroupPermissionsModule, DocumentsModule, DocumentGroupsModule],
   controllers: [AppController],
   providers: [AppService],
 })
