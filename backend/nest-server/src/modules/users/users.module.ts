581b3072132885f3b6b453192c46ce8f9351b2b7
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-07-28 20:06:31 +0200

    Add whoami api for testing api

--- backend/nest-server/src/modules/users/users.module.ts
+++ backend/nest-server/src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { SessionModule } from '../auth/session/session.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), SessionModule],
   exports: [TypeOrmModule, UsersService],
   controllers: [UsersController],
   providers: [UsersService],
