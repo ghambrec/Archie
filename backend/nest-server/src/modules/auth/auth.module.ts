988b6e09776f8d8feaa703abf4b0cbd8effd6d6d
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-07-28 19:45:00 +0200

    Implement login endpoint

--- backend/nest-server/src/modules/auth/auth.module.ts
+++ backend/nest-server/src/modules/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SessionService } from './session/session.service';
+import { SessionCookieService } from './session/session-cookie.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, SessionService]
+  providers: [AuthService, SessionService, SessionCookieService],
 })
 export class AuthModule {}
--- backend/nest-server/src/modules/auth/auth.service.ts
+++ backend/nest-server/src/modules/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { SessionService } from './session/session.service';
+import { LoginRequestDto } from './dto/login-request.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
+import { LoginResult } from './interfaces/login-result.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly sessionService: SessionService,
+  ) {}
+  
+  async login(request: LoginRequestDto) : Promise<LoginResult> {
+    const user = await this.userService.findByEmail(request.email); // you'll need to add this
+    if (!user || !(await bcrypt.compare(request.password, user.passwordHash))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const sessionId = await this.sessionService.create(user.id);
+    return { user, sessionId };
+  }
+  
+}
