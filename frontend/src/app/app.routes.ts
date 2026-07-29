1212547913f7747a7f74b318668ff225bc0f0185
ghambrec <gabriel.hambrecht@gmail.com>
2026-07-29 18:46:57 +0200

    add: frontend basic strcuture with sidebar, home and settings

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Settings } from './settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
+	{
+		path: 'home',
+		component: Home,
+		title: 'Home',
+	},
+	{
+		path: 'settings',
+		component: Settings,
+		title: 'Settings',
+	},
+];
---
+++ frontend/src/app/users/create-user-form/create-user-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user-form',
+  imports: [],
+  templateUrl: './create-user-form.html',
+  styleUrl: './create-user-form.scss',
+})
+export class CreateUserForm {}
