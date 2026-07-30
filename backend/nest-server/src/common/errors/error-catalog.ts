44e878da7a1fee68c195181738d9f359d9c1a397
poverbec <poverbec@student.42heilbronn>
2026-07-30 16:09:36 +0200

    nest server common error class for common errors

---
+++ backend/nest-server/src/common/errors/error-catalog.ts
@@ -0,0 +1,34 @@
+import { HttpStatus } from "@nestjs/common";
+import { ErrorCode } from "./error-code";
+
+export interface ErrorDefinition{
+	status: HttpStatus;
+	message:string;
+}
+
+export const ERROR_CATALOG ={
+	[ErrorCode.EmailAlreadyRegistered]: {
+      status: HttpStatus.CONFLICT,
+      message: 'Email is already registered.',
+    },
+
+    [ErrorCode.InvalidCredentials]: {
+      status: HttpStatus.UNAUTHORIZED,
+      message: 'Invalid credentials.',
+    },
+
+    [ErrorCode.UserNotFound]: {
+      status: HttpStatus.NOT_FOUND,
+      message: 'User was not found.',
+    },
+
+	[ErrorCode.UserNameAlreadyRegistered]: {
+		status: HttpStatus.CONFLICT,
+		message: 'Display Name is already registered.'
+	},
+	[ErrorCode.InteralServerError]: {
+		status: HttpStatus.INTERNAL_SERVER_ERROR,
+		message: 'An unexpected error occurred.',
+	},
+  } satisfies Record<ErrorCode, ErrorDefinition>;
+
---
+++ backend/nest-server/src/common/errors/error-code.ts
@@ -0,0 +1,14 @@
+
+
+export enum ErrorCode{
+	EmailAlreadyRegistered =
+      'AUTH_EMAIL_ALREADY_REGISTERED',
+    InvalidCredentials =
+      'AUTH_INVALID_CREDENTIALS',
+    UserNotFound =
+      'USER_NOT_FOUND',
+	UserNameAlreadyRegistered =
+	 'USER_NAME_ALREADY_REGISTERED',
+	InteralServerError =
+	 'SERVER_CONFIG_ERROR'
+}
