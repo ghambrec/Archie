df7a1e603c760139b77d9d7a80932e3bde6039ff
ghambrecht <gabriel.hambrecht@gmail.com>
2026-08-01 14:41:36 +0200

    add: frontend sidebar now functional

--- frontend/src/app/sidebar/sidebar.ts
+++ frontend/src/app/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
