e3d54ab7a1dd49665ebd3b1e71ee0e18a98b746d
ghambrecht <gabriel.hambrecht@gmail.com>
2026-08-02 16:57:41 +0200

    add frontend: base files for create group modal

--- frontend/src/app/groups/group-list/group-list.ts
+++ frontend/src/app/groups/group-list/group-list.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-group-list',
-  imports: [],
-  templateUrl: './group-list.html',
-  styleUrl: './group-list.scss',
+	selector: 'app-group-list',
+	imports: [],
+	templateUrl: './group-list.html',
+	styleUrl: './group-list.scss',
 })
 export class GroupList {}
