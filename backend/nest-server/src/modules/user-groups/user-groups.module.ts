73395a67a4099dca82b627a016cfa74d07966378
Cedric Bauer <cbauer@2-D-5.42heilbronn.de>
2026-08-03 17:33:19 +0200

    ADD: Up to date version of user-group.module.ts

--- backend/nest-server/src/modules/user-groups/user-groups.module.ts
+++ backend/nest-server/src/modules/user-groups/user-groups.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UserGroupsService } from './user-groups.service';
+import { UserGroupsController } from './user-groups.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserGroup } from './entities/user-group.entity';
+import { User } from '../users/entities/user.entity';
+import { UsersModule } from '../users/users.module';
+import { GroupsModule } from '../groups/groups.module';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserGroup]), UsersModule, GroupsModule],
+  providers: [UserGroupsService],
+  controllers: [UserGroupsController],
+  exports: [UserGroupsService],
+})
 export class UserGroupsModule {}
