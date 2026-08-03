6ebfc04d75bd70bd8a9f1527b0dcc7b990067d27
Cedric Bauer <cbauer@2-D-5.42heilbronn.de>
2026-08-03 15:05:12 +0200

    ADD: create-groups.dto.ts

--- backend/nest-server/src/modules/groups/dto/create-groups.dto.ts
+++ backend/nest-server/src/modules/groups/dto/create-groups.dto.ts
@@ -1,14 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsBoolean, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateGroupsDto {
-  @ApiProperty({ example: 'group_name'})
+  @ApiProperty({ example: 'group_4'})
   @IsString()
+  @IsNotEmpty()
   name!: string;
 
   @ApiProperty({ example: 'readonly group'})
-  description!: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
 
-  @ApiProperty({ example: 'false'})
-  isSystem!: string;
+  @ApiProperty({ example: false})
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) => value === 'true' || value === true )
+  isSystem?: boolean = false;
 
 }
