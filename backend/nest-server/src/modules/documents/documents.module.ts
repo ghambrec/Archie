0b07b71d1b2be38faa120869137ec87afd343e4e
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-06 16:25:26 +0200

    Add to documents module SessionModule dependense

--- backend/nest-server/src/modules/documents/documents.module.ts
+++ backend/nest-server/src/modules/documents/documents.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { SessionModule } from '../auth/session/session.module';
 
 @Module({
-  controllers: [DocumentsController],
+  controllers: [DocumentsController, SessionModule],
   providers: [DocumentsService]
 })
+
 export class DocumentsModule {}
