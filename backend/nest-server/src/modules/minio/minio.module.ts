dda42517283e20b65fb0c8f9a5f6229ddb263669
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-06 16:55:55 +0200

    Create miio provider

--- backend/nest-server/src/modules/minio/minio.module.ts
+++ backend/nest-server/src/modules/minio/minio.module.ts
@@ -1,4 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { Client } from 'minio';
+import { getEnv, getEnvBoolean, getEnvNumber, requireEnv } from '../../common/env/env';
 
-@Module({})
+export const MINIO_CLIENT = 'MINIO_CLIENT';
+
+const DEFAULT_MINIO_ENDPOINT = 'minio';
+const DEFAULT_MINIO_PORT = 9000;
+const DEFAULT_MINIO_USE_SSL = false;
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: MINIO_CLIENT,
+      useFactory: () => {
+        return new Client({
+          endPoint: getEnv('MINIO_ENDPOINT', DEFAULT_MINIO_ENDPOINT),
+          port: getEnvNumber('MINIO_PORT', DEFAULT_MINIO_PORT),
+          useSSL: getEnvBoolean('MINIO_USE_SSL', DEFAULT_MINIO_USE_SSL),
+          accessKey: requireEnv('MINIO_APP_ACCESS_KEY'),
+          secretKey: requireEnv('MINIO_APP_SECRET_KEY'),
+        });
+      },
+    },
+  ],
+  exports: [MINIO_CLIENT],
+})
 export class MinioModule {}
