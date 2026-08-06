4ae27daacd77174100f8c06c2476ee897dbd4c76
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-06 16:46:50 +0200

    Add minio module

--- backend/nest-server/src/app.module.ts
+++ backend/nest-server/src/app.module.ts
@@ -13,6 +13,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { RedisModule } from './modules/redis/redis.module';
 import { GroupsController } from './modules/groups/groups.controller';
 import { AppLoggerModule } from './logger/app-logger.module';
+import { MinioModule } from './modules/minio/minio.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { AppLoggerModule } from './logger/app-logger.module';
     RedisModule,
     AuthModule,
     AppLoggerModule,
+    MinioModule,
   ],
   controllers: [AppController, GroupsController],
   providers: [AppService],
