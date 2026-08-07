9ea7cfbe0595cc5b4baf8d92c61cf261894f0404
Cedric Bauer <cbauer@3-C-12.42heilbronn.de>
2026-08-07 15:20:50 +0200

    ADD: adding guards to endpoints; changing URL for getting group by Name

--- backend/nest-server/src/modules/groups/groups.controller.ts
+++ backend/nest-server/src/modules/groups/groups.controller.ts
@@ -5,6 +5,7 @@ import { CreateGroupsResponseDto } from './dto/create-groups-response.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { Group } from './entities/group.entity';
 import { UpdateGroupsDto } from './dto/update-groups.dto';
+import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
 
 @ApiTags('groups')
 @Controller('groups')
@@ -12,26 +13,31 @@ export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
   @Post('create')
+  @UseGuards(SessionAuthGuard)
   async create(@Body() dto: CreateGroupsDto): Promise<CreateGroupsResponseDto> {
     return this.groupsService.create(dto);
   }
 
   @Get(':id')
+  @UseGuards(SessionAuthGuard)
   async get(@Param('id') id: string): Promise<Group> {
     return this.groupsService.get(id);
   }
 
-  @Get('by-name/:name')
+  @Get(':name')
+  @UseGuards(SessionAuthGuard)
   async getByName(@Param('name') name: string): Promise<Group> {
     return this.groupsService.getByNameOrFail(name);
   }
 
   @Get()
+  @UseGuards(SessionAuthGuard)
   async findAll(): Promise<Group[]> {
     return this.groupsService.findAll();
   }
 
   @Patch(':id')
+  @UseGuards(SessionAuthGuard)
   async update(
     @Param('id') id: string, 
     @Body() dto: UpdateGroupsDto): Promise<Group> {
@@ -48,7 +54,7 @@ export class GroupsController {
   // }
 
   @Delete(':id')
-  // @UseGuards(SessionAuthGuard)
+  @UseGuards(SessionAuthGuard)
   async remove(@Param('id') id: string): Promise<void> {
     await this.groupsService.deleteGroup(id);
     return;
