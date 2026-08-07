c953dc2d4f02663a626edabd5fbfaf710cb2a9b0
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-07 20:00:22 +0200

    Use file service in controller for endpoint

--- backend/nest-server/src/modules/users/users.module.ts
+++ backend/nest-server/src/modules/users/users.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersFileService } from './users-file.service';
 import { SessionModule } from '../auth/session/session.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), SessionModule],
+  imports: [TypeOrmModule.forFeature([User]), SessionModule, StorageModule],
   exports: [TypeOrmModule, UsersService],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersFileService],
 })
 export class UsersModule {}
