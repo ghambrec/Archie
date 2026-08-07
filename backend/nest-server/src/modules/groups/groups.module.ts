a0bda4f27b85a32a7330f0eeea8491e3f06a2420
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-07 18:49:37 +0200

    Add SessionModule to groups.module.ts

--- backend/nest-server/src/modules/groups/groups.module.ts
+++ backend/nest-server/src/modules/groups/groups.module.ts
@@ -3,9 +3,10 @@ import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from './entities/group.entity';
+import { SessionModule } from '../auth/session/session.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group]), SessionModule],
   exports: [TypeOrmModule, GroupsService],
   controllers: [GroupsController],
   providers: [GroupsService],
