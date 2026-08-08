ddf875d6121e3202bcdc16b3c51aa198f94e45e3
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-08 19:41:57 +0200

    Add logs to auth service

--- backend/nest-server/src/modules/auth/auth.service.ts
+++ backend/nest-server/src/modules/auth/auth.service.ts
@@ -7,30 +7,36 @@ import { LoginResult } from './interfaces/login-result.interface';
 import { RegisterRequestDto } from './dto/register-request.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UserSummaryDto } from '../users/dto/user-summary.dto';
+import { Logger } from 'nestjs-pino';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly logger: Logger,
     private readonly userService: UsersService,
     private readonly sessionService: SessionService,
   ) {}
-  
+
   async login(request: LoginRequestDto) : Promise<LoginResult> {
+    this.logger.debug('Execute login', request.email);
+
     const user = await this.userService.findByEmail(request.email);
     if (!user || !(await bcrypt.compare(request.password, user.passwordHash))) {
+      this.logger.warn(`Login failed for ${request.email}`);
       throw new UnauthorizedException('Invalid credentials');
     }
 
     const sessionId = await this.sessionService.create(user.id);
     return { user, sessionId };
   }
-  
+
   async register(request: RegisterRequestDto) : Promise<string> {
-    
+    this.logger.debug('Execute register', request.email);
+
     const userExists = await this.userService.findByEmail(request.email)
     if(userExists)
       throw new ConflictException(`Email ${request.email} already exists`);
-    
+
     const createUserDto: CreateUserDto = {
       email: request.email,
       password: request.password,
@@ -42,13 +48,15 @@ export class AuthService {
     const sessionId = await this.sessionService.create(user.id);
     return sessionId;
   }
-  
+
   async logout(sessionId: string) : Promise<void> {
+    this.logger.debug('Execute logout', sessionId);
 
     return this.sessionService.destroy(sessionId);
   }
 
   async getCurrentUser(userId: string) : Promise<UserSummaryDto> {
+    this.logger.debug('Execute getCurrentUser', userId);
     return this.userService.findProfileById(userId);
   }
 }
