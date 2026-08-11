c262f7709293770dcbaccdc224c0c3b280382cc5
Cedric Bauer <cbauer@2-D-1.42heilbronn.de>
2026-08-11 15:46:22 +0200

    ADD: added admin-groups.controller.ts for better visibility

---
+++ backend/nest-server/src/modules/groups/admin-groups.controller.ts
@@ -0,0 +1,53 @@
+import { Controller, Body, Post, UseGuards, Delete, Get, Patch, Param, ParseUUIDPipe, Req } from '@nestjs/common';
+import { GroupsService } from './groups.service';
+import { CreateGroupsDto } from './dto/create-groups.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { UpdateGroupsDto } from './dto/update-groups.dto';
+import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
+import { AdminRequiredGuard } from '../permissions/guards/admin-required.guard';
+import { GroupsAdminResponseDto } from './dto/groups-admin-response';
+
+@ApiTags('admin/groups')
+@Controller('admin/groups')
+export class AdminGroupsController {
+  constructor(private readonly groupsService: GroupsService) {}
+
+  @Post('create')
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async create(@Body() dto: CreateGroupsDto): Promise<GroupsAdminResponseDto> {
+    return this.groupsService.adminCreate(dto);
+  }
+
+  @Get(':id')
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async get(@Param('id', new ParseUUIDPipe()) id: string): Promise<GroupsAdminResponseDto> {
+    return this.groupsService.adminGet(id);
+  }
+
+  @Get('by-name/:name')
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async getByName(@Param('name') name: string): Promise<GroupsAdminResponseDto> {
+    return this.groupsService.adminGetByNameOrFail(name);
+  }
+
+  @Get()
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async findAll(): Promise<GroupsAdminResponseDto[]> {
+    return this.groupsService.adminFindAll();
+  }
+
+  @Patch(':id')
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async update(
+    @Param('id') id: string, 
+    @Body() dto: UpdateGroupsDto): Promise<GroupsAdminResponseDto> {
+    return this.groupsService.adminUpdate(id, dto); 
+  }
+
+  @Delete(':id')
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
+  async remove(@Param('id') id: string, @Req() req: Request & { userId: string }): Promise<void> {
+    await this.groupsService.adminDeleteGroup(id, req.userId, true);
+    return;
+  }
+}
