e506c3756d11e1aae7742bfbf7d4ee779b743d8a
poverbec <poverbec@student.42heilbronn>
2026-08-11 09:26:42 +0200

    nest server getenv fixed special cases

--- backend/nest-server/src/common/env/env.ts
+++ backend/nest-server/src/common/env/env.ts
@@ -2,12 +2,18 @@ export function getEnv(key: string, defaultValue: string): string {
   return process.env[key] ?? defaultValue;
 }
 
-export function getEnvNumber(key: string, defaultValue: number): number {
+export function getEnvNumber(
+  key: string,
+  defaultValue: number
+  ): number {
   const value = process.env[key];
+  if(value == undefined || value?.trim() ==  '' )
+    return defaultValue;
   const parsed = Number(value);
+  
   if(!Number.isInteger(parsed))
     throw new Error ( ` env error:  " ${value} " musst be a number`); 
-  return value ? Number(value) : defaultValue;
+  return value ? parsed : defaultValue;
 }
 
 export function getEnvBoolean(key: string, defaultValue: boolean): boolean {
@@ -15,9 +21,9 @@ export function getEnvBoolean(key: string, defaultValue: boolean): boolean {
   if (value == undefined || value.trim() =='' )
       return defaultValue
   const parsed = value.trim().toLocaleLowerCase();
-  if(parsed !== 'true' && parsed !== 'false')
+  if(parsed.trim().toLowerCase() !== 'true' && parsed.trim().toLowerCase() !== 'false')
     throw new Error (` env error ${value}, need to be a boolean`);
-  return true;
+  return parsed === 'true';
 }
 
 export function requireEnv(key: string): string {
