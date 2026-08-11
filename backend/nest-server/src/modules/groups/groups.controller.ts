174d0f7ce2e1fe8bdf2431060b336d1b44242e85
Cedric Bauer <cbauer@2-D-1.42heilbronn.de>
2026-08-11 15:47:39 +0200

    MODIFY: Deleted admin-group logic from this file and advanced the logic for deleting groups by including isSystem check and if user is member of a group check

--- backend/nest-server/src/modules/groups/groups.controller.ts
+++ backend/nest-server/src/modules/groups/groups.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Body, Post, UseGuards, Delete, Get, Patch, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Body, Post, UseGuards, Delete, Get, Patch, Param, ParseUUIDPipe, Req } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { CreateGroupsDto } from './dto/create-groups.dto';
 import { GroupsResponseDto } from './dto/groups-response.dto';
 import { ApiTags } from '@nestjs/swagger';
-import { Group } from './entities/group.entity';
 import { UpdateGroupsDto } from './dto/update-groups.dto';
 import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
-import { AdminRequiredGuard } from '../permissions/guards/admin-required.guard';
 import { GroupsAdminResponseDto } from './dto/groups-admin-response';
+
+interface AuthenticatedRequest extends Request {
+  userId: string;
+}
 
 @ApiTags('groups')
 @Controller('groups')
@@ -57,53 +59,8 @@ export class GroupsController {
 
   @Delete(':id')
   @UseGuards(SessionAuthGuard)
-  async remove(@Param('id') id: string): Promise<void> {
-    await this.groupsService.deleteGroup(id);
-    return;
-  }
-}
-
-@ApiTags('admin/groups')
-@Controller('admin/groups')
-export class AdminGroupsController {
-  constructor(private readonly groupsService: GroupsService) {}
-
-  @Post('create')
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async create(@Body() dto: CreateGroupsDto): Promise<GroupsAdminResponseDto> {
-    return this.groupsService.adminCreate(dto);
-  }
-
-  @Get(':id')
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async get(@Param('id', new ParseUUIDPipe()) id: string): Promise<GroupsAdminResponseDto> {
-    return this.groupsService.adminGet(id);
-  }
-
-  @Get('by-name/:name')
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async getByName(@Param('name') name: string): Promise<GroupsAdminResponseDto> {
-    return this.groupsService.adminGetByNameOrFail(name);
-  }
-
-  @Get()
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async findAll(): Promise<GroupsAdminResponseDto[]> {
-    return this.groupsService.adminFindAll();
-  }
-
-  @Patch(':id')
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async update(
-    @Param('id') id: string, 
-    @Body() dto: UpdateGroupsDto): Promise<GroupsAdminResponseDto> {
-    return this.groupsService.adminUpdate(id, dto); 
-  }
-
-  @Delete(':id')
-  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  async remove(@Param('id') id: string): Promise<void> {
-    await this.groupsService.adminDeleteGroup(id);
+  async remove(@Param('id') id: string,  @Req() req: AuthenticatedRequest ): Promise<void> {
+    await this.groupsService.deleteGroup(id, req.userId, false);
     return;
   }
 }
