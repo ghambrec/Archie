15548de65dc502edab2b61b456bdba08d0bfefab
Cedric Bauer <cbauer@2-E-1.42heilbronn.de>
2026-08-11 10:02:57 +0200

    MODIFY: fixed getGroupsByName endpoint bug; Protected main endpoints via Admin authentification

--- backend/nest-server/src/modules/groups/groups.controller.ts
+++ backend/nest-server/src/modules/groups/groups.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post, UseGuards, Delete, Get, Patch, Param, DefaultValuePipe, ParseIntPipe } from '@nestjs/common';
+import { Controller, Body, Post, UseGuards, Delete, Get, Patch, Param, ParseUUIDPipe } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { CreateGroupsDto } from './dto/create-groups.dto';
 import { CreateGroupsResponseDto } from './dto/create-groups-response.dto';
@@ -6,6 +6,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { Group } from './entities/group.entity';
 import { UpdateGroupsDto } from './dto/update-groups.dto';
 import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
+import { AdminRequiredGuard } from '../permissions/guards/admin-required.guard';
 
 @ApiTags('groups')
 @Controller('groups')
@@ -13,18 +14,18 @@ export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
   @Post('create')
-  @UseGuards(SessionAuthGuard)
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   async create(@Body() dto: CreateGroupsDto): Promise<CreateGroupsResponseDto> {
     return this.groupsService.create(dto);
   }
 
   @Get(':id')
   @UseGuards(SessionAuthGuard)
-  async get(@Param('id') id: string): Promise<Group> {
+  async get(@Param('id', new ParseUUIDPipe()) id: string): Promise<Group> {
     return this.groupsService.get(id);
   }
 
-  @Get(':name')
+  @Get('by-name/:name')
   @UseGuards(SessionAuthGuard)
   async getByName(@Param('name') name: string): Promise<Group> {
     return this.groupsService.getByNameOrFail(name);
@@ -37,7 +38,7 @@ export class GroupsController {
   }
 
   @Patch(':id')
-  @UseGuards(SessionAuthGuard)
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   async update(
     @Param('id') id: string, 
     @Body() dto: UpdateGroupsDto): Promise<Group> {
@@ -54,10 +55,11 @@ export class GroupsController {
   // }
 
   @Delete(':id')
-  @UseGuards(SessionAuthGuard)
+  @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   async remove(@Param('id') id: string): Promise<void> {
     await this.groupsService.deleteGroup(id);
     return;
   }
-
+  
 }
+
