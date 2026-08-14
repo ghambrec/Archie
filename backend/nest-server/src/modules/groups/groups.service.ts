305c41d186001027bac94d22d829f3fe4fab2bbd
Cedric Bauer <cbauer@2-E-1.42heilbronn.de>
2026-08-14 12:10:59 +0200

    ADD: adding logger statements for group logic for regular user and admin user

--- backend/nest-server/src/modules/groups/groups.service.ts
+++ backend/nest-server/src/modules/groups/groups.service.ts
@@ -1,12 +1,16 @@
-import { Injectable, ConflictException, NotFoundException, ForbiddenException } from '@nestjs/common';
+import { Injectable, NotFoundException, ForbiddenException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Group } from './entities/group.entity';
 import { Repository } from 'typeorm';
 import { CreateGroupsDto } from './dto/create-groups.dto';
 import { GroupsResponseDto } from './dto/groups-response.dto';
 import { UpdateGroupsDto } from './dto/update-groups.dto';
-import { GroupsAdminResponseDto } from './dto/groups-admin-response';
 import { UserGroup } from '../user-groups/entities/user-group.entity';
+import { Logger } from 'nestjs-pino';
+import { isUserMemberOfGroup } from './groups.helper';
+import { findGroupByName } from './groups.helper';
+
+// REGULAR USER LOGIC
 
 @Injectable()
 export class GroupsService {
@@ -14,12 +18,18 @@ export class GroupsService {
     @InjectRepository(Group)
     private readonly groupsRepository: Repository<Group>,
     @InjectRepository(UserGroup)
-    private readonly userGroupsRepository: Repository<UserGroup>
+    private readonly userGroupsRepository: Repository<UserGroup>,
+    private readonly logger: Logger
   ) {}
+
   async create(dto: CreateGroupsDto): Promise<GroupsResponseDto> {
-    const nameTaken = await this.findByName(dto.name)
-    if(nameTaken)
-        throw new NotFoundException('Group not found');
+    this.logger.log({ groupName: dto.name }, 'User is creating group')
+
+    const nameTaken = await findGroupByName(this.groupsRepository, dto.name)
+    if(nameTaken) {
+      this.logger.warn({ groupName: dto.name }, 'Group name is already taken');
+      throw new NotFoundException('Group not found');
+    }
 
     const groupEntity = this.groupsRepository.create({
       name: dto.name,
@@ -27,32 +37,40 @@ export class GroupsService {
     });
 
     await this.groupsRepository.save(groupEntity);
-      
+
+    this.logger.log({ grouId: groupEntity.id, groupName: groupEntity.name }, 'User created group successfully');
     return new GroupsResponseDto(groupEntity);
   }
 
-  async findByName(name: string): Promise<Group | null> {
-    return this.groupsRepository.findOneBy({name: name});
-  }
-
   async get(id: string): Promise<GroupsResponseDto> {
+    this.logger.log({ groupId: id }, 'User is fetching group by id');
+
     const group = await this.groupsRepository.findOneBy({id});
     if (!group || group.isSystem) {
+      this.logger.warn({ groupId: id }, 'Group not found or is system group');
       throw new NotFoundException('Cannot create group');
     }
+
+    this.logger.log({ groupId: group.id, groupName: group.name }, 'User fetching group successful');
     return new GroupsResponseDto(group);
   }
 
   async getByNameOrFail(name: string): Promise<GroupsResponseDto> {
-    const group = await this.findByName(name);
+    this.logger.log({ groupName: name }, 'User fetching group by name');
+
+    const group = await findGroupByName(this.groupsRepository, name);
 
     if (!group || group.isSystem) {
+      this.logger.warn({ groupName: name }, 'Group not found or is system group')
       throw new NotFoundException('Group not found');
     }
+
+    this.logger.log({ groupId: group.id, groupName: group.name }, 'User fetching group successfully');
     return new GroupsResponseDto(group);
   }
 
   async findAll(): Promise<GroupsResponseDto[]> {
+    this.logger.log('User fetching all accessible groups');
     const groups = await this.groupsRepository.find({
       where: { isSystem: false }
     });
@@ -67,12 +85,15 @@ export class GroupsService {
   // }
 
   async update(id: string, dto: UpdateGroupsDto): Promise<GroupsResponseDto> {
+    this.logger.log({ groupId: id }, 'User is updating group');
+
     const group = await this.get(id);
 
     if ( dto.name && dto.name !== group.name) {
-      const nameTaken = await this.findByName(dto.name);
+      const nameTaken = await findGroupByName(this.groupsRepository, dto.name);
       if (nameTaken) {
-        throw new ConflictException('Name is already taken');
+        this.logger.warn({ groupName: dto.name }, 'Group name is already taken');
+        throw new NotFoundException('Group not found');
       }
     }
 
@@ -82,117 +103,40 @@ export class GroupsService {
     }
     const updatedGroup = await this.get(id);
 
+    this.logger.log({ groupId: updatedGroup.id, groupName: updatedGroup.name }, 'User updating group successful');
     return updatedGroup;
   }
 
   async deleteGroup(id: string, userId: string, skipMembershipCheck: boolean): Promise<void> {
+    this.logger.log({ groupId: id, userId: userId }, 'User trying to delete group');
+    
     const group = await this.groupsRepository.findOneBy({ id });
     if (!group) {
+      this.logger.warn({ groupId: id }, 'Group not found');
       throw new NotFoundException('Group not found');
     }
     
-    // check if user is still in group?
     if (group.isSystem == true) {
+      this.logger.warn({ groupId: group.id }, 'Group is system group');
       throw new NotFoundException('Group not found');
     }
     if ( !skipMembershipCheck )
-    {
-      const isMember = await this.isUserMemberOfGroup(userId, group.id);
+      {
+      const isMember = await isUserMemberOfGroup(this.userGroupsRepository, userId, group.id); // check if user is still in group?
       if (isMember == false) {
+        this.logger.warn({ groupId: group.id, userId: userId }, 'User is not a member of this group');
         throw new ForbiddenException('You are not a member of this group');
       }
     }
 
     await this.groupsRepository.remove(group);
 
+    this.logger.log({ groupId: group.id, userId: userId }, 'User successfully deleted group');
     console.log(`Group ${group.id} (${group.name}) has been deleted`);
   }
 
-
-  // ADMIN LOGIC
-
-  async adminCreate(dto: CreateGroupsDto): Promise<GroupsAdminResponseDto> {
-    
-    const nameTaken = await this.findByName(dto.name)
-    if(nameTaken)
-        throw new ConflictException('Name is already taken');
-
-    const groupEntity = this.groupsRepository.create({
-      name: dto.name,
-      description: dto.description,
-    });
-
-    await this.groupsRepository.save(groupEntity);
-
-    return new GroupsAdminResponseDto(groupEntity);
-  }
-
-  async adminGet(id: string): Promise<GroupsAdminResponseDto> {
-    const group = await this.groupsRepository.findOneBy({ id });
-    if (!group) {
-      throw new NotFoundException(`Group with id ${id} not found`);
-    }
-    
-    return new GroupsAdminResponseDto(group);
-  }
-
-  async adminGetByNameOrFail(name: string): Promise<GroupsAdminResponseDto> {
-    const group = await this.findByName(name);
-
-    if (!group) {
-      throw new NotFoundException(`Group with the name "${name}" not found`);
-    }
-    return new GroupsAdminResponseDto(group);
-  }
-
-  async adminFindAll(): Promise<GroupsAdminResponseDto[]> {
-    const groups = this.groupsRepository.find();
-    return (await groups).map( group => new GroupsAdminResponseDto(group));
-  }
-
-  async adminUpdate(id: string, dto: UpdateGroupsDto): Promise<GroupsAdminResponseDto> {
-    const group = await this.adminGet(id);
-
-    if ( dto.name && dto.name !== group.name) {
-      const nameTaken = await this.findByName(dto.name);
-      if (nameTaken) {
-        throw new ConflictException('Name is already taken');
-      }
-    }
-    const result = await this.groupsRepository.update(id, dto);
-    if ( result.affected === 0 ) {
-      throw new NotFoundException(`Group ${id} not found`)
-    }
-    const updatedGroup = await this.adminGet(id);
-
-    return updatedGroup;
-  }
-
-  async adminDeleteGroup(id: string, userId: string, skipMembershipCheck: boolean): Promise<void> {
-    const group = await this.groupsRepository.findOneBy({ id });
-    if (!group) {
-      throw new NotFoundException(`Group with id ${id} not found`);
-    }
-    
-    if (group.isSystem == true) {
-      throw new ForbiddenException('System groups cannot be deleted');
-    }
-
-    if ( !skipMembershipCheck )
-    {
-      const isMember = await this.isUserMemberOfGroup(userId, group.id);
-      if (isMember == false) {
-        throw new ForbiddenException('You are not a member of this group');
-      }
-    }
-
-    await this.groupsRepository.remove(group);
-
-    console.log(`Group ${group.id} (${group.name}) has been deleted`);
-  }
-
-  private async isUserMemberOfGroup(userId: string, groupId: string): Promise<boolean> {
-    const membership = await this.userGroupsRepository.findOneBy({userId, groupId});
-    return !!membership;
-  }
 }
+
+//colors for log messages
+// separte admin in seperate file
+// delete case
