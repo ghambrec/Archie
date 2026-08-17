e1510122a435fd3f64fa33f4f41bc4027daf3e0e
poverbec <poverbec@student.42heilbronn>
2026-08-17 10:52:28 +0200

    actions

--- frontend/src/app/groups/group-list/group-list.ts
+++ frontend/src/app/groups/group-list/group-list.ts
@@ -3,22 +3,26 @@ import { TranslocoPipe } from '@jsverse/transloco';
 import { Groups } from '../groups';
 
 
-@Component({
-  selector: 'app-group-list',
-  imports: [TranslocoPipe],
-  templateUrl: './group-list.html',
-  styleUrl: './group-list.scss',
-})
-export class GroupList implements OnInit {
-  protected readonly groupsService = inject(Groups):
+//@Component({
+//  selector: 'app-group-list',
+//  imports: [TranslocoPipe],
+//  templateUrl: './group-list.html',
+//  styleUrl: './group-list.scss',
+//})
+//export class GroupList implements OnInit {
+//  protected readonly groupsService = inject(Groups):
 
-  ngOnInit():void {
-    this.loadGroups(),
-  }
+//  ngOnInit():void {
+//    this.loadGroups(),
+//  }
 
-  private loadGroups(): void {
-    this.groupsService.getGroups().subscripbe({
-      
-    })
-  }
-}
+//  private loadGroups(): void {
+//    this.groupsService.getGroups().subscribe({
+//      next: Groups => {
+//        this.groupsService.groupsList.set(Groups)
+//      }
+//      //logger?
+//      //error:
+//    })
+//  }
+//}
