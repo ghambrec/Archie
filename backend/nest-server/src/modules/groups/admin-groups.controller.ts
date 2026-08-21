f3303b56620034c8c81db6ead225812d8a18b9eb
Cedric Bauer <cbauer@2-E-3.42heilbronn.de>
2026-08-21 11:42:38 +0200

    ADD: adding descriptions to endpoints of admin-groups.controller.ts

--- backend/nest-server/src/modules/groups/admin-groups.controller.ts
+++ backend/nest-server/src/modules/groups/admin-groups.controller.ts
@@ -16,9 +16,9 @@ export class AdminGroupsController {
 
   @Post('create')
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  @ApiOperation({
-        summary: '',
-        description: '.'
+    @ApiOperation({
+        summary: 'ADMIN USER: Creates group',
+        description: 'This endpoint is only accessible by an admin user and creates a group. You set name and description in the body and it returns you id, name, description and isSystem bool.'
       })
   async create(@Body() dto: CreateGroupsDto): Promise<GroupsAdminResponseDto> {
     return this.adminGroupsService.adminCreate(dto);
@@ -26,10 +26,10 @@ export class AdminGroupsController {
 
   @Get(':id')
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
-  @ApiOperation({
-        summary: '',
-        description: '.'
-      })
+   @ApiOperation({
+      summary: 'ADMIN USER: Get group by id',
+      description: 'This endpoint is only accessible by an admin user and gets you information about one group. You enter the groupId for the group you want. It returns you id, name, description and isSystem bool about the group.'
+    })
   async get(@Param('id', new ParseUUIDPipe()) id: string): Promise<GroupsAdminResponseDto> {
     return this.adminGroupsService.adminGet(id);
   }
@@ -37,9 +37,9 @@ export class AdminGroupsController {
   @Get('by-name/:name')
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   @ApiOperation({
-        summary: '',
-        description: '.'
-      })
+      summary: 'ADMIN USER: Gets group by name',
+      description: 'This endpoint is only accessible by an admin user and does the same as getting group by id, but instead you enter the name of the group and it returns you id, name, description and isSystem bool about the group.'
+    })
   async getByName(@Param('name') name: string): Promise<GroupsAdminResponseDto> {
     return this.adminGroupsService.adminGetByNameOrFail(name);
   }
@@ -47,9 +47,9 @@ export class AdminGroupsController {
   @Get()
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   @ApiOperation({
-        summary: '',
-        description: '.'
-      })
+      summary: 'ADMIN USER: Gets you all fetchable groups',
+      description: 'This endpoint is only accessible by an admin user and is returning all groups visible to the regular user, even system groups.'
+    })
   async findAll(): Promise<GroupsAdminResponseDto[]> {
     return this.adminGroupsService.adminFindAll();
   }
@@ -57,9 +57,9 @@ export class AdminGroupsController {
   @Patch(':id')
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   @ApiOperation({
-        summary: '',
-        description: '.'
-      })
+      summary: 'ADMIN USER: Modifies a group',
+      description: 'This endpoint is only accessible by an admin user and allows a regular user to enter an groupId and modify the name and description about this group.'
+    })
   async update(
     @Param('id') id: string, 
     @Body() dto: UpdateGroupsDto): Promise<GroupsAdminResponseDto> {
@@ -69,9 +69,9 @@ export class AdminGroupsController {
   @Delete(':id')
   @UseGuards(SessionAuthGuard, AdminRequiredGuard)
   @ApiOperation({
-        summary: '',
-        description: '.'
-      })
+      summary: 'ADMIN USER: Deletes group',
+      description: 'This endpoint is only accessible by an admin user and allows a admin user to delete a fetchable group based on the groupId he is entering - even groups he is no part of. Returning only response headers.'
+    })
   async remove(@Param('id') id: string, @Req() req: Request): Promise<void> {
     await this.adminGroupsService.adminDeleteGroup(id, req.userId!, true);
     return;
