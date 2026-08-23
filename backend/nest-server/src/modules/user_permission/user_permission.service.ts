8165bc792bc67c8ff8d8bf0b8a1d9b506c8b2a84
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-08-23 17:51:59 +0200

    Create function hasPermission to user_permissions.service

--- backend/nest-server/src/modules/user_permission/user_permission.service.ts
+++ backend/nest-server/src/modules/user_permission/user_permission.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserPermission } from './entities/user_permission.entity';
 
 @Injectable()
-export class UserPermissionService {}
+export class UserPermissionService {
+  constructor(
+    @InjectRepository(UserPermission)
+    private readonly userPermissionRepository: Repository<UserPermission>,
+  ) {}
+
+  async hasPermission(
+    userId: string,
+    groupId: string,
+    permKey: string,
+  ): Promise<boolean> {
+    return this.userPermissionRepository
+      .createQueryBuilder('userPermission')
+      .innerJoin(
+        'permissions',
+        'permission',
+        'permission.id = userPermission.permission_id',
+      )
+      .where('userPermission.user_id = :userId', { userId })
+      .andWhere('userPermission.group_id = :groupId', { groupId })
+      .andWhere('permission.perm_key = :permKey', { permKey })
+      .getExists();
+  }
+}
