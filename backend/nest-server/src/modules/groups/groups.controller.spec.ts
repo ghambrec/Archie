d558fb4f6f3e5fc99503d0bcf2a81c8f86d4bc63
Cedric Bauer <cbauer@3-C-6.42heilbronn.de>
2026-08-27 13:48:14 +0200

    ADD: Adding unit-tests to groups.controller.spec.ts && .service.spec.ts

--- backend/nest-server/src/modules/groups/groups.controller.spec.ts
+++ backend/nest-server/src/modules/groups/groups.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupsController } from './groups.controller';
+import { GroupsService } from './groups.service';
+import { SessionAuthGuard } from '../auth/guards/session-auth.guard';
 
 describe('GroupsController', () => {
   let controller: GroupsController;
+  let service: jest.Mocked<GroupsService>; // type annotation
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GroupsController],
-    }).compile();
+      providers: [
+        {
+          provide: GroupsService,
+          useValue: { // actual mock
+            create: jest.fn(),
+            get: jest.fn(),
+            findAll: jest.fn(),
+            update: jest.fn(),
+            deleteGroup: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(SessionAuthGuard)
+      .useValue({ canActivate: jest.fn(() => true )})
+      .compile();
 
-    controller = module.get<GroupsController>(GroupsController);
+    controller = module.get(GroupsController);
+    service = module.get(GroupsService); // mock service
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('POST /groups/create calls service', async () => {
+    // Arrange testdaten & Mockverhalten definiert
+    const dto = { name: 'A', description: 'B' };
+    const expected = { id: '1', ...dto };
+    service.create.mockResolvedValue(expected);
+
+    // act: call echter controller 
+    const result = await controller.create(dto);
+
+    // assert: check
+    expect(result).toEqual(expected);
+    expect(service.create).toHaveBeenCalledWith(dto);
   });
+
 });
+
+//test with: npm test -- groups.controller.spec.ts
--- backend/nest-server/src/modules/groups/groups.service.spec.ts
+++ backend/nest-server/src/modules/groups/groups.service.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupsService } from './groups.service';
+import { Repository } from 'typeorm';
+import { Group } from './entities/group.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { GroupsResponseDto } from './dto/groups-response.dto';
+import { createMockGroup } from './test-utils/group.factory';
+import { UserGroup } from '../user-groups/entities/user-group.entity';
+import { provideMockRepository } from './test-utils/mock-repositories';
+import { Logger } from 'nestjs-pino';
 
 describe('GroupsService', () => {
   let service: GroupsService;
+  let repo: jest.Mocked<Repository<Group>>;
+  let userGroupRepo: jest.Mocked<Repository<UserGroup>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GroupsService],
+      providers: [
+        GroupsService,
+        provideMockRepository(Group),
+        provideMockRepository(UserGroup),
+        {
+          provide: Logger, 
+          useValue: {
+            log: jest.fn(),
+            warn: jest.fn(),
+            error: jest.fn(),
+            debug: jest.fn(),
+            verbose: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<GroupsService>(GroupsService);
+    repo = module.get(getRepositoryToken(Group));
+    userGroupRepo = module.get(getRepositoryToken(UserGroup));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('create() should create a group and return it', async () => {
+    //Arrange
+    const dto = { name: 'Test', description: 'Desc' };
+    const savedGroup = createMockGroup({ id: 'uuid-1', ...dto });
+    repo.findOneBy.mockResolvedValue(null);
+    repo.create.mockReturnValue(savedGroup as any);
+    repo.save.mockResolvedValue(savedGroup as any);
+    
+    //Act
+    const result = await service.create(dto);
+
+    //Assert
+    expect(result).toEqual(new GroupsResponseDto(savedGroup));
+    expect(repo.save).toHaveBeenCalledWith(savedGroup);
   });
+
+  it('create() should throw if name already exists', async () => {
+    repo.findOneBy.mockResolvedValue(createMockGroup({ id: 'x', name: 'Test' }));
+    await expect(service.create({ name: 'Test', description: '' })).rejects.toThrow(NotFoundException);
+  });
+
+  //get
+  it('should return group if found', async () => {
+    const group = createMockGroup({ id: 'g1' });
+    repo.findOneBy.mockResolvedValue(group);
+
+    const result  = await service.get('g1');
+
+    expect(result).toEqual(new GroupsResponseDto(group));
+    expect(repo.findOneBy).toHaveBeenCalledWith({ id: 'g1' });
+  });
+
+  it('should reject when group does not exist', async () => {
+    repo.findOneBy.mockResolvedValue(null);
+
+    await expect
+  })
+
 });
+
+//test with: npm test -- groups.service.spec.ts
