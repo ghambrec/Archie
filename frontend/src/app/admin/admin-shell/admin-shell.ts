e7b6557927664341a01f70f800127d75c19a0a85
Cedric Bauer <cbauer@2-E-5.42heilbronn.de>
2026-09-01 09:59:11 +0200

    ADD: admin-shell.ts && admin-shell.html work in progress

---
+++ frontend/src/app/admin/admin-shell/admin-shell.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-admin-shell',
+  templateUrl: './admin-shell.html',
+  styleUrl: './admin-shell.scss',
+})
+export class AdminShell {} 
