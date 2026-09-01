d578e108260a9e131bdebdafaabb2a9cea807ef3
Cedric Bauer <cbauer@2-E-5.42heilbronn.de>
2026-09-01 09:58:39 +0200

    ADD: added routes for the admin UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { Home } from './home/home';
 import { Settings } from './settings/settings';
 import { Shell } from './shell/shell';
 import { Login } from './auth/login/login';
-import { authGuard, guestGuard } from './auth/auth-guard';
+import { adminGuard, authGuard, guestGuard } from './auth/auth-guard';
 import { Profile } from './profile/profile';
 import { Documents } from './documents/documents';
+import { AdminShell } from './admin/admin-shell/admin-shell';
+import { AdminUsers } from './admin/admin-users/admin-users'; 
+import { AdminGroups } from './admin/admin-groups/admin-groups';
+import { AdminSettings } from './admin/admin-settings/admin-settings';
 
 export const routes: Routes = [
 	{
@@ -46,4 +50,28 @@ export const routes: Routes = [
 			},
 		],
 	},
+	{
+		path: 'admin',
+		component: AdminShell,
+		canActivate: [authGuard, adminGuard],
+		children: [
+			{
+				path: '',
+				redirectTo: 'users',
+				pathMatch: 'full',
+			},
+			{
+				path: 'users',
+				component: AdminUsers
+			},
+			{
+				path: 'groups',
+				component: AdminGroups
+			},
+			{ 
+				path: 'settings',
+				component: AdminSettings
+			},
+		],
+	}
 ];
