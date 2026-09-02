66d01a73f6a0e162cbad32d266e479d9b1b10269
Cedric Bauer <cbauer@2-E-1.42heilbronn.de>
2026-09-02 15:15:37 +0200

    ADD: implemented endpoint for admin adding group

--- frontend/src/app/groups/groups.ts
+++ frontend/src/app/groups/groups.ts
@@ -2,29 +2,44 @@ import { Service, signal, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-
-
-export interface GroupInfo {
+export interface GroupResponseAdmin {
 	id: string;
 	name: string;
 	description: string | null;
+	isSystem: boolean;
+}
+
+export interface CreateGroupDto {
+	name: string;
+	description?: string | null;
 }
 
 @Service()
 export class Groups {
 	private readonly http = inject(HttpClient);
-	private readonly baseUrl = `${environment.apiUrl}/groups`;
+	private readonly baseUrl = `${environment.apiUrl}/admin/groups`;
 	
-	readonly groupsList = signal<GroupInfo[]>([]);
+	readonly groupsList = signal<GroupResponseAdmin[]>([]);
 	
-	getGroups() {
-		return this.http.get<GroupInfo[]>(
+	getGroupsAdmin() {
+		return this.http.get<GroupResponseAdmin[]>(
 			this.baseUrl,
 			{
 				withCredentials: true,
 			}
 		);
 	}
+
+	createGroupAdmin(dto: CreateGroupDto) {
+		return this.http.post<GroupResponseAdmin>(`${this.baseUrl}/create`,
+			dto,
+			{
+				withCredentials: true
+			}
+		);
+	}
+
+	// deleteGroupAdmin() {
+	// 	return this.http.
+	// }
 }
-
-
