0c67fb96be3b985818b55d464a434fadbe3b5305
Cedric Bauer <cbauer@2-E-1.42heilbronn.de>
2026-09-03 09:26:26 +0200

    ADD: deleteGroupModal.ts file

---
+++ frontend/src/app/groups/delete-group-modal/delete-group-modal.ts
@@ -0,0 +1,43 @@
+import { Component, inject, signal } from "@angular/core";
+import { FormField, FormRoot } from "@angular/forms/signals";
+import { TranslocoPipe } from "@jsverse/transloco";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { GroupResponseAdmin, Groups } from "../groups";
+
+@Component({
+  selector: 'app-delete-group-modal',
+	imports: [FormRoot, TranslocoPipe],
+	templateUrl: './delete-group-modal.html',
+	styleUrl: './delete-group-modal.scss',
+})
+export class DeleteGroupModal {
+  selectedGroup!: GroupResponseAdmin;
+
+  protected readonly activeModal = inject(NgbActiveModal);
+  private readonly groupsService = inject(Groups);
+
+  readonly errorMsg = signal<string | null>(null);
+
+  confirmDelete(): void {
+    this.groupsService.deleteGroupAdmin(this.selectedGroup.id).subscribe({
+      next: () => {
+        this.activeModal.close();
+      },
+      error: (error) => {
+        let translocoKey = 'groups.deleteGroup.errorGeneral';
+
+        if (error.status === 403) {
+          translocoKey = 'groups.deleteGroup.errorForbidden';
+        } else if (error.status === 404) {
+          translocoKey = 'groups.deleteGroup.errorNotFound';
+        }
+
+        this.errorMsg.set(translocoKey);
+      },
+    });
+  }
+
+  closeModal(): void {
+    this.activeModal.dismiss();
+  }
+}
